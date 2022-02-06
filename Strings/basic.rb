190cc158586c9f1a22da02fc002f033ec059bd04
ashish negi <ashu.negi@oncehub.com>
2022-02-06 21:51:30 +0530

    String session code

---
+++ Strings/basic.rb
@@ -0,0 +1,17 @@
+# STRING
+
+# xyz = 'I\'m Ashish Negi'
+# puts xyz
+
+# puts "sum of two numbers is #{3+3}"
+
+# new = 'THiS iS StrINg'
+# puts new.downcase
+# puts new.upcase
+# puts new.capitalize
+# puts new.length
+
+#############################################################
+
+
+
